fa9b76ef53f88a003f5c14e5a9bf56a54d6b7869
Wahyudi <core@wahyudi.dev>
2024-01-08 03:03:00 +0800

    what ever

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="min-h-screen max-w-3xl mx-auto bg-black flex justify-center items-center">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
