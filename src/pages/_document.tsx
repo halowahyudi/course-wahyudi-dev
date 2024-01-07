a863c456736fb5f18338aae385299c033dbf4c6f
Wahyudi <core@wahyudi.dev>
2024-01-08 03:07:05 +0800

    what ever

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Course</title>
+      </Head>
       <body className="min-h-screen max-w-3xl mx-auto bg-black flex justify-center items-center">
         <Main />
         <NextScript />
